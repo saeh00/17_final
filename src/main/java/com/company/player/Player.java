4959610986a8cbce76b17e1162cc87216256fcf2
eminolcer <Eminolcer@gmail.com>
2021-01-11 19:11:30 +0100

    Jeg har lavet en player klasse

---
+++ src/main/java/com/company/player/Player.java
@@ -0,0 +1,39 @@
+package com.company.player;
+
+
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+
+import java.awt.*;
+
+public class Player {
+
+    private final GUI_Player gui_player;
+
+    private final String playerName;
+
+    public Player(GUI gui, Color color) {
+
+        playerName = gui.getUserString("input player name")
+        GUI_Car car = new GUI_Car();
+        car.setPrimaryColor(color);
+
+        gui_player = new GUI_Player(playerName,30000,car);
+
+        gui.addPlayer(gui_player);
+
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public GUI_Player getGui_player() {
+        return gui_player;
+    }
+
+    public void movePlayer(int distance){
+
+    }
+}
