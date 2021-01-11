c3410faed402673bd23c0a2a52408a795b5317cd
Omar <s205471@outook.dk>
2021-01-11 18:40:36 +0100

    Jeg har oprettet de forskellige klasser som extender fra vores field klasse

---
+++ src/main/java/com/company/fields/Field.java
@@ -0,0 +1,14 @@
+package com.company.fields;
+
+abstract class Field {
+
+    String name,subText;
+
+    public Field(String name, String subText) {
+        this.name = name;
+        this.subText = subText;
+
+    }
+
+
+}
