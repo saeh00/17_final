ea8f7911190aa794b0afd4951e68220c7cbb55cc
saeh00 <saufi2810@gmail.com>
2021-01-13 12:41:25 +0100

    tester om spillet kører i main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,19 +1,14 @@
 package com.company;
 
+import com.company.fields.FieldGUI;
 import gui_main.GUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        GUI gui = new GUI();
+        GameController controller = new GameController(new GUI(FieldGUI.guiFieldsFactory(FieldGUI.fields)));
 
-
-
-
-
-
-
-
+        controller.boardSetup();
 
 
         
